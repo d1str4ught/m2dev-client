15c823cf7a37a42bd9af0967f49b527d9c0e1618
ErLullo <contelorenzo90@gmail.com>
2026-01-07 19:30:37 +0100

    Update consolemodule.py

--- assets/root/consolemodule.py
+++ assets/root/consolemodule.py
@@ -740,7 +740,7 @@ class ConsoleWindow(ui.Window):
 	def CloseWindow(self):
 		self.Hide()
 
-	## NOTE : �̰����� Command�� ó���մϴ� - [levites]
+	## NOTE : 이곳에서 Command를 처리합니다 - [levites]
 	def ProcessCommand(self, text):
 
 		if '/' == text[0]:
